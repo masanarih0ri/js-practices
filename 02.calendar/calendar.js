61cb3e121f733b6e61490ef4cb49d14d50248322
masanarih0ri <masanarihoridevelop@gmail.com>
2022-03-08 22:30:43 +0900

    fix lint

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,8 +1,8 @@
-const argv = require('minimist')(process.argv.slice(2));
+const argv = require('minimist')(process.argv.slice(2))
 const year = argv.y ? argv.y : new Date().getFullYear()
 const month = argv.m ? argv.m : new Date().getMonth() + 1
 const weekday = ['日', '月', '火', '水', '木', '金', '土']
-const month_last_day = new Date(year, month, 0).getDate()
+const monthLastDay = new Date(year, month, 0).getDate()
 
 const header = () => {
   return `      ${month}月  ${year}`
@@ -11,7 +11,7 @@ const header = () => {
 const weekdays = () => {
   let weekdays = ''
   for (let i = 0; i < weekday.length; i++) {
-    suffix = weekday[i] === '土' ? '' : ' '
+    const suffix = weekday[i] === '土' ? '' : ' '
     weekdays += `${weekday[i]}${suffix}`
   }
   return weekdays
@@ -19,32 +19,32 @@ const weekdays = () => {
 
 const days = () => {
   let day = 1
-  let display_days = ''
-  display_days += first_blank()
-  for (let i = 1; i <= month_last_day; i++) {
-    display_days += display_day(day)
+  let displayDays = ''
+  displayDays += firstBlank()
+  for (let i = 1; i <= monthLastDay; i++) {
+    displayDays += displayDay(day)
     day++
   }
-  return display_days
+  return displayDays
 }
 
-const display_day = (day) => {
-  const suffix = is_sunday(day) ? "\n" : ' '
-  return  `${padding(day.toString(), 2)}${suffix}`
+const displayDay = (day) => {
+  const suffix = isSunday(day) ? '\n' : ' '
+  return `${padding(day.toString(), 2)}${suffix}`
 }
 
 const padding = (target, n) => String(target).padStart(n, ' ')
 
-const is_sunday = (day) => {
+const isSunday = (day) => {
   return new Date(`${year}-${month}-${day}`).getDay() === 6
 }
 
-const first_blank = () => {
+const firstBlank = () => {
   let blank = ''
   // 月の初日の曜日を表す数値を取得することで、余白の計算に利用する
-  const repeat_count = new Date(`${year}-${month}-1`).getDay()
-  if (repeat_count === 0) return ''
-  for (let i = 0; i < repeat_count; i++) {
+  const repeatCount = new Date(`${year}-${month}-1`).getDay()
+  if (repeatCount === 0) return ''
+  for (let i = 0; i < repeatCount; i++) {
     blank += '   '
   }
   return blank
